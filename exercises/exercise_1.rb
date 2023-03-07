f007c0dac1a6b655af2c5662ab0f0f4dbee64b74
Denny Doan <denny-333@hotmail.com>
2023-03-07 12:17:49 -0500

    Completed excercise 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,8 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-burnaby = Store.new(location: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.new(location: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.new(location: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+burnaby = Store.create(location: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(location: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(location: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts "There are #{Store.count.to_s} in the db"
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,9 +7,9 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-surrey = Store.new(location: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
-whistler = Store.new(location: 'Whistler', annual_revenue: 1900000, mens_apparel: false, womens_apparel: false)
-yaletown = Store.new(location: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+surrey = Store.create(location: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(location: 'Whistler', annual_revenue: 1900000, mens_apparel: false, womens_apparel: false)
+yaletown = Store.create(location: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 @mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
 
