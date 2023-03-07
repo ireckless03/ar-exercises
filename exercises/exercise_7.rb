6c41453be59ffc9e771e654fc01fb17339a25c09
Denny Doan <denny-333@hotmail.com>
2023-03-07 12:42:23 -0500

    Completed excercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,16 +10,34 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
-# Add validations to two models to enforce the following business rules:
-# Employees must always have a first name present
-# Employees must always have a last name present
-# Employees have a hourly_rate that is a number (integer) between 40 and 200
-# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
-# Stores must always have a name that is a minimum of 3 characters
-# Stores have an annual_revenue that is a number (integer) that must be 0 or more
-# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
-# Ask the user for a store name (store it in a variable)
-# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
-# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+class Employee < ApplicationRecord
+  belongs_to :store 
 
-class Employee
+  validates :first_name, :last_name, :hourly_rate, :store_id, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+end
+
+class Store < ApplicationRecord
+  has_many: employees
+
+  validates :location, length: { minimum: 3}
+  validates :annual_revenue, numericality: { minimum: 0}
+  validates :carries_apparel
+
+  def carries_apparel
+    if mens_apparel.present? || womens_apparel.present?
+    end
+  end
+
+end
+
+puts "Enter store location"
+store_location = gets.chomp
+
+if store_location = Store.create(location: store_location)
+  puts "#{store_location} was created"
+else
+  puts "Did not create sucessfully"
+end
+
+
